ffb16db490cc945870660869ecbca4ec34b47bcf
jakeparker7 <jacobcolinparker@hotmail.com>
2020-02-15 18:44:54 +1000

    small change

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -9,7 +9,7 @@ class UserPage extends Component {
         <main className="container">
         <div className="row acc-title p-4 ml-2">
           <div className="col-lg-9 text-left pl-1 mt-3">
-            <h6>Your Platform Subs: 1500</h6>
+            <h6>My Points: 20,000</h6>
           </div>
           <div className="col-lg-3 text-right pl-1 mt-3">
             <h6>Your Points: 20,000</h6>
