22e6cfa35db54aeee63f00d4dfe2733a02249659
Leo Vigna <leo@joinsaturn.com>
2020-02-16 00:40:14 -0700

    Updates

---
+++ precache-manifest.36d317e72cd82a662871b97c50c6d8d6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c4b277ed1ead12b6d6167029c4bdd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098f986bd6a71dac436c",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "617e639d16c2ef96c155",
+    "url": "/static/css/main.20348b4c.chunk.css"
+  },
+  {
+    "revision": "098f986bd6a71dac436c",
+    "url": "/static/js/2.487af5f0.chunk.js"
+  },
+  {
+    "revision": "765b2db94433745a0cbbfc272d6e258b",
+    "url": "/static/js/2.487af5f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617e639d16c2ef96c155",
+    "url": "/static/js/main.ece9c348.chunk.js"
+  },
+  {
+    "revision": "92f246d45c8d75a688da",
+    "url": "/static/js/runtime-main.2be98f9c.js"
+  },
+  {
+    "revision": "359224f32f6330e46a9bb44ae5d93781",
+    "url": "/static/media/bk-overview.359224f3.png"
+  },
+  {
+    "revision": "60cc68accb3f0ff2e7e6070d2526dc00",
+    "url": "/static/media/coin.60cc68ac.png"
+  },
+  {
+    "revision": "fd552de2d259a0b1306f7a0cc9c57c66",
+    "url": "/static/media/ins.fd552de2.png"
+  },
+  {
+    "revision": "5aebd41d3b9089e57e91ec57bee3f6d0",
+    "url": "/static/media/lion.5aebd41d.png"
+  },
+  {
+    "revision": "c8a61fab229f09e1c5ab698fe3cbb56f",
+    "url": "/static/media/side-icons.c8a61fab.png"
+  },
+  {
+    "revision": "65b6556b251d6cd4df48389e013af138",
+    "url": "/static/media/tw.65b6556b.png"
+  },
+  {
+    "revision": "36937bf084f24ccb8dae33a6af1d6ddf",
+    "url": "/static/media/twitch.36937bf0.png"
+  },
+  {
+    "revision": "3479109f06d32de721a8cf34c8e5d1c7",
+    "url": "/static/media/yt.3479109f.png"
+  }
+]);
