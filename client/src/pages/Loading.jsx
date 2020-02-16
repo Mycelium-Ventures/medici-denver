dfc9431ec6367527891d1c20f03bd3f1f96755d9
jakeparker7 <jacobcolinparker@hotmail.com>
2020-02-16 15:40:56 +1000

    removed 3box changed loader

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { CircularProgress } from '@material-ui/core'
 import styled from 'styled-components'
+import {HashLoader} from "react-spinners";
+
 
 const StyledLoading = styled.div`
   padding-top: 30%;
@@ -11,17 +13,15 @@ class Loading extends Component {
 
   render() {
     return (
-      <StyledLoading className="App body">
-        <div className="row">
-          <div className="col text-right" style={{paddingTop: '8px'}}>
-            Loading
-          </div>
-          <div className="col text-left">
-            <CircularProgress />
-          </div>
+      <div className="container m-5" style={{height: "80vh !important"}}>
+        <div class="d-flex justify-content-center h-100">
+            <HashLoader
+              size={150}
+              color={"#ce60a5"}
+              loading={true}
+            />
         </div>
-
-      </StyledLoading>
+      </div>
     )
   }
 }
